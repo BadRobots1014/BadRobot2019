cc1f73ca986fa40446d873ff6ad3afbb7a77cbb9
Alex Herridge <alherridge@gmail.com>
2019-02-03 22:17:43 -0500

    Added Hatch Height Constants

---
+++ src/main/java/frc/robot/HatchHeight.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class HatchHeight
+{
+    public static final double ROCKET_HIGH = 2;
+    public static final double ROCKET_MED = 1;
+    public static final double ROCKET_LOW = 0;
+    public static final double CARGO_SHIP = 0.2;
+}
