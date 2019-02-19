830dba31525a75a5538cfc445f4d921d89694448
jrAlex <alherridge@gmail.com>
2019-02-19 12:50:49 -0500

    Fixed Logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,34 @@
 package frc.robot;
 
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import badlog.lib.BadLog;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class Robot extends TimedRobot
 {
+  private BadLog logger;
+
   @Override
   public void robotInit()
   {
-    BadLog log = BadLog.init("/home/lvuser/" + UUID.randomUUID() + ".badbag");
+    var timestamp = new SimpleDateFormat("dd-MM-yyyy-HH-mm").format(new Date());
+    logger = BadLog.init("/home/lvuser/" + timestamp + ".badbag");
+
+    BadLog.createValue("Match Type", DriverStation.getInstance().getMatchType().toString());
+    BadLog.createValue("Match Number", "" + DriverStation.getInstance().getMatchNumber());
+    BadLog.createTopic("Match Time", "s", DriverStation.getInstance()::getMatchTime);
+
+    BadLog.createValue("OS Version", System.getProperty("os.version"));
+    BadLog.createTopic("Free Memory", "Bytes", () -> (double) Runtime.getRuntime().freeMemory());
 
     Subsystems.init();
     Controls.init();
 
-    BadLog.createValue("OS Version", System.getProperty("os.version"));
-    BadLog.createTopic("Free Memory", "Bytes", () -> (double) Runtime.getRuntime().freeMemory());
-    log.finishInitialization();
+    logger.finishInitialization();
   }
 
   @Override
@@ -59,6 +69,12 @@ public class Robot extends TimedRobot
   public void teleopPeriodic()
   {
     Scheduler.getInstance().run();
+
+    if (!DriverStation.getInstance().isDisabled())
+    {
+      logger.updateTopics();
+      logger.log();
+    }
   }
 
   @Override
